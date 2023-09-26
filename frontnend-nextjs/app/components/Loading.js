e7b17a6b47f0f4bd4bf3cfcb3fa8b74de49c5cd9
Jumaru <spiralout.eu@gmail.com>
2023-09-26 18:30:10 +0300

    make loading as the recycle icon

--- frontnend-nextjs/app/components/Loading.js
+++ frontnend-nextjs/app/components/Loading.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaRecycle } from 'react-icons/fa';
 
 const Loading = ({ visible }) => {
   // Use the "visible" prop to control the visibility of the loading component
@@ -9,7 +10,10 @@ const Loading = ({ visible }) => {
   return (
     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-opacity-40 bg-white">
       <div className="relative">
-        <div className="animate-spin rounded-full h-24 w-24 border-t-8 border-b-8 border-blue-500"></div>
+      <div className="animate-spin">
+        <FaRecycle className="h-16 w-16 text-gray-900" />
+      </div>
+        {/* <div className="animate-spin rounded-full h-24 w-24 border-t-8 border-b-8 border-blue-500"></div> */}
       </div>
     </div>
   );
